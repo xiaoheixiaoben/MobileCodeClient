d1fc09c1840b6d98d6d3fcce21dd1be68d0a7a63
xiaoheixiaoben <32826863+xiaoheixiaoben@users.noreply.github.com>
2017-10-16 11:17:26 +0800

    Initial commit

---
+++ JavaSource/cn/com/test/Test.java
@@ -0,0 +1,22 @@
+package cn.com.test;
+
+import cn.com.webxml.MobileCodeWS;
+import cn.com.webxml.MobileCodeWSSoap;
+
+/**
+ * 
+ * @author Administrator
+ * @date 2017-10-16
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		MobileCodeWS mobileCodeWS = new MobileCodeWS();
+		MobileCodeWSSoap soap = mobileCodeWS.getMobileCodeWSSoap();
+		String mobileCodeInfo = soap.getMobileCodeInfo("1372007", "");
+
+		System.out.println(mobileCodeInfo);
+
+	}
+
+}
